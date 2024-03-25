f7dce22396645ef2e6e37c8855f781990daf9933
student4-1 <164903113+student4-1@users.noreply.github.com>
2024-03-25 10:39:02 -0500

    Parameters posted

--- Group_project.cpp
+++ Group_project.cpp
@@ -1,8 +1,11 @@
 /*
-Topic: 
+Topic: Create a menu-based console application that acts as a programming tutor for users.
 Authors:
 Due Date:
 */
+//Eight sections: Introduction, Key Concepts, Code Templates, Troubleshooting Guide,
+//Advanced Applications, Quiz, Hands-on Exercises, and Resources and Links.
+//Our section is Dictionaries, Advanced Python Concepts(our choice), and Libraries.
 #include<iostream>
 
 using namespace std;
